08220f0959f55474c664975155e794fd92a50a5a
BM <bmbashar063@gmail.com>
2024-07-05 15:09:35 +0600

    assignment one done

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+$num1 =4;
+$num2 =43;
+$num3 =6;
+
+if ($num1 > $num2){
+    if ( $num1 > $num3){
+        echo "The Largest Number Is: $num1";
+    }
+        else{
+            echo "The Largest Number Is: $num3";
+        }
+}
+ if ($num2 > $num1){
+    if ( $num2 > $num3){
+        echo "The Largest Number Is: $num2";
+    }
+    else {
+        echo "The Largest Number Is: $num3";
+    }
+ }
